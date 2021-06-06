6a0d0b9349887d8ab3dcec49ea43800892fd71d7
yogesh889 <8058yogesh@gmail.com>
2021-06-06 23:35:28 +0530

    Jun 6 11:35 PM

--- conversions/octal_to_decimal.cpp
+++ conversions/octal_to_decimal.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binaryTodecimal(int n){
+int octalTodecimal(int n)
+{
     int ans = 0;
     int x = 1;
     while(n>0)
     {
         int y = n%10;
         ans += x*y;
-        x *=2;
+        x *=8;
         n /=10;
     }
     return ans;
@@ -18,6 +19,6 @@ int32_t main(){
     int n;
     cin >> n;
      
-    cout << binaryTodecimal(n) << endl;
+    cout << octalTodecimal(n) << endl;
 
 }
