9c6f5dca4c1edfa3eec661c5fbc7c664188a1709
yogesh889 <8058yogesh@gmail.com>
2021-06-08 01:24:11 +0530

    Jun 7 11:49 PM

--- conversions/decimal_to_hexadecimal.cpp
+++ conversions/decimal_to_hexadecimal.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 string decimalTohexadecimal(int n)
 {
-    string ans = "";
     int x = 1;
-    while(x<=n)
+    string ans = "";
+    while(x <= n)
         x *=16;
-    x/=16;
+    x /=16;
 
     while(x>0)
     {
         int lastdigit = n/x;
         n -= lastdigit*x;
-        x/=16;
+        x /=16;
 
         if(lastdigit <= 9)
             ans = ans + to_string(lastdigit);
