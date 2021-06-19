e7a7d41336c1094c5ec23125023e84f33a2665a2
yogesh889 <8058yogesh@gmail.com>
2021-06-19 23:58:03 +0530

    Jun 19 11:57PM

--- arraychallenges/length_of_longest_subarray.cpp
+++ arraychallenges/length_of_longest_subarray.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main(){
 
-    int n;
+    int n, i;
     cin>>n;
-    
-    int arr[n]
+
+    int arr[n];
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
 
     int ans = 2;
-    int pd = arr[1]-arr[0];c   
+    int pd = arr[1]-arr[0]; 
     int j=2;
     int curr = 2;
     while(j<n){
